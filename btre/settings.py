2d7d8d5ad97d6a21e3a7260738eebc641ef52820
lyk2 <infoover2@gmail.com>
2019-11-14 14:58:11 +0200

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -149,3 +149,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'testemail@gm.com'
 EMAIL_HOST_PASSWORD = 'testpassword'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
